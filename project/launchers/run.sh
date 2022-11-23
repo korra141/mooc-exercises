22264e466e7e4062e1fc189c4d4093a1966908d4
Ala Eddine <alaeddine_limeme@hotmail.com>
2022-11-23 16:04:15 -0500

    Updated run.sh to use standalone_test launch file

--- project/launchers/run.sh
+++ project/launchers/run.sh
@@ -16,6 +16,6 @@ dt-exec-BG roslaunch --wait car_interface all.launch veh:=$VEHICLE_NAME
 # Will activate this once we run the full demo
 #dt-exec-FG roslaunch --wait duckietown_demos communication.launch || true
 # For the moment let's just run our specific node since we dont need any other one for testing
-dt-exec-FG roslaunch --wait communication communication_node.launch || true
+dt-exec-FG roslaunch --wait communication communication_node_standalone_test.launch || true
 
 copy-ros-logs

9c5e134fa41be6adb6d6363c4f05716df4375b0a
Ria Arora <31388414+korra141@users.noreply.github.com>
2022-12-31 22:20:21 +0200

    bug fix 2

--- collision/solution/collision_checker.py
+++ collision/solution/collision_checker.py
@@ -41,15 +41,15 @@ def check_collision(environment: List[PlacedPrimitive], robot_body: List[PlacedP
     #Put placed primites in world frame 
     # You can start by rototranslating the robot_body by the robot_pose
     rototranslated_robot: List[PlacedPrimitive] = []
-    phi = robot_pose.theta
+    phi = robot_pose.theta_deg
     for robot_parts in robot_body: 
-        robot_frame = np.array([robot_parts.pose.x,robot_parts.pose.y,1)]
+        robot_frame = np.array([robot_parts.pose.x,robot_parts.pose.y,1])
         roto_translate = np.array([[np.cos(phi),-np.sin(phi), robot_pose.x],
                                    [np.sin(phi), np.cos(phi), robot_pose.y],
                                    [0,0,1]])
         world_frame = np.dot(roto_translate,robot_frame)
-        pose_new = new FriendlyPose(world_frame[0],world_frame[1],robot_parts.pose.theta + phi)
-        rototranslated_robot.append(new PlacedPrimitive(pose_new,robot_parts.primitive))
+        pose_new = FriendlyPose(world_frame[0],world_frame[1],robot_parts.pose.theta_deg + phi)
+        rototranslated_robot.append(PlacedPrimitive(pose_new,robot_parts.primitive))
         
         
     # == WRITE ME ==
@@ -74,26 +74,26 @@ def distance_between(a,b):
                                
 def radii_rectangle(a: PlacedPrimitive)
     # inscribed and circumscribed circles
-    return min(a.ymin+a.ymax,a.xmin + a.xmax)/2, distance_between(np.array([xmax,ymax]),np.array([xmin,ymin]))/2
+    return min(a.ymin+a.ymax,a.xmin + a.xmax)/2, distance_between(np.array([a.xmax,a.ymax]),np.array([a.xmin,a.ymin]))/2
 
 def corners_of_rectangle(a: PlacedPrimitive):
     corners = []
-    corners.append(np.array([a.pose.x + a.pose.primitive.xmax,a.pose.y + a.pose.primitive.ymax]))
-    corners.append(np.array([a.pose.x + a.pose.primitive.xmax,a.pose.y - a.pose.primitive.ymin]))
-    corners.append(np.array([a.pose.x - a.pose.primitive.xmin,a.pose.y + a.pose.primitive.ymax]))
-    corners.append(np.array([a.pose.x - a.pose.primitive.xmin,a.pose.y - a.pose.primitive.ymin]))
+    corners.append(np.array([a.pose.x + a.primitive.xmax,a.pose.y + a.primitive.ymax]))
+    corners.append(np.array([a.pose.x + a.primitive.xmax,a.pose.y - a.primitive.ymin]))
+    corners.append(np.array([a.pose.x - a.primitive.xmin,a.pose.y + a.primitive.ymax]))
+    corners.append(np.array([a.pose.x - a.primitive.xmin,a.pose.y - a.primitive.ymin]))
     
     return corners
                                
 def circle_collision(pose1,pose2,radius1,radius2):
-    bool collision_check = False
+    collision_check = False
     if(0 <= distance_between(np.array([pose1.x,pose1.y]),np.array([pose2.x,pose2.y])) <= (radius1 + radius2)):
         collision_check =  True
     return collision_check
 
 def check_collision_shape(a: PlacedPrimitive, b: PlacedPrimitive) -> bool:
     # This is just some code to get you started, but you don't have to follow it exactly
-    bool is_collided = False
+    is_collided = False
 
     # This is just some code to get you started, but you don't have to follow it exactly
     if isinstance(a.primitive, Circle) and isinstance(b.primitive, Circle):
